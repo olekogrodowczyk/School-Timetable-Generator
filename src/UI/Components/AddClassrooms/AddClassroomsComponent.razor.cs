446cc45e9cdc6cc1223cb428c15e081f063ecd72
ogrodowczyko <ollo9998@gmail.com>
2021-12-04 18:16:34 +0100

    Seperate pages into components

---
+++ src/UI/Components/AddClassrooms/AddClassroomsComponent.razor.cs
@@ -0,0 +1,32 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace UI.Components.AddClassrooms
+{
+    public partial class AddClassroomsComponent
+    {
+        protected string value = String.Empty;
+
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        public ILocalStorageService LocalStorageService { get; set; }
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JSRuntime.InvokeVoidAsync("initializeAddClass");
+            }
+        }
+
+        protected async Task GetJson()
+        {
+            value = await LocalStorageService.GetItemAsync<string>("MyClasses");
+        }
+    }
+}
