991515e67ad02e2948ec75c6f6b5c19da098f381
ogrodowczyko <ollo9998@gmail.com>
2021-12-13 03:31:36 +0100

    Add Viewmodels

---
+++ src/Shared/ViewModels/ClassVm.cs
@@ -0,0 +1,25 @@
+﻿using Application.Mappings;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.ViewModels
+{
+    public class ClassVm : IMap
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TimetableId { get; set; }
+        public TeacherVm Teacher { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Class, ClassVm>()
+                .ForMember(x => x.Teacher, opt => opt.MapFrom(y => y.Teacher));
+        }
+    }
+}
---
+++ src/Shared/ViewModels/TeacherVm.cs
@@ -0,0 +1,22 @@
+﻿using Application.Mappings;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.ViewModels
+{
+    public class TeacherVm : IMap
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Teacher, TeacherVm>();
+        }
+    }
+}
