dee3baddf344dbbc09ac7511446b88294f6fa0b1
ogrodowczyko <ollo9998@gmail.com>
2021-12-14 01:58:08 +0100

    Add teacher to group entity

--- src/Domain/Entities/Group.cs
+++ src/Domain/Entities/Group.cs
@@ -13,6 +13,7 @@ namespace Domain.Entities
         public virtual Class Class { get; set; }
         public string Name { get; set; }
         public int? SubjectId { get; set; }
+        public int TeacherId { get; set; }
         public virtual Subject Subject { get; set; }
         public virtual ICollection<Assignment> Assignments { get; set; }
         public virtual ICollection<Lesson> Lessons { get; set; }
---
+++ src/Infrastructure/Migrations/20211214005742_addTeacherToGroup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class addTeacherToGroup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TeacherId",
+                table: "Groups",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeacherId",
+                table: "Groups");
+        }
+    }
+}
