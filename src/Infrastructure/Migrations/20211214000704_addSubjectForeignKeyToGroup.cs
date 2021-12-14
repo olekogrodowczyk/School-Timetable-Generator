2bbe6a112a3030b9fcf9bbd7b85c4cb388a9d1ee
ogrodowczyko <ollo9998@gmail.com>
2021-12-14 01:08:16 +0100

    Update database

---
+++ src/Infrastructure/Migrations/20211214000704_addSubjectForeignKeyToGroup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class addSubjectForeignKeyToGroup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SubjectId",
+                table: "Groups",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_SubjectId",
+                table: "Groups",
+                column: "SubjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_Subjects_SubjectId",
+                table: "Groups",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Subjects_SubjectId",
+                table: "Groups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Groups_SubjectId",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "SubjectId",
+                table: "Groups");
+        }
+    }
+}
