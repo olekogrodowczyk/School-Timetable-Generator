869c4d7bb5de978c04a79a0011582925b84195ba
ogrodowczyko <ollo9998@gmail.com>
2021-12-15 06:20:59 +0100

    Fix availability creating

--- src/Application/Services/AvailabilityService.cs
+++ src/Application/Services/AvailabilityService.cs
@@ -15,19 +15,22 @@ namespace Application.Services
     {
         private readonly IMapper _mapper;
         private readonly IAvailabilityRepository _availabilityRepository;
+        private readonly IUserRepository _userRepository;
 
-        public AvailabilityService(IMapper mapper, IAvailabilityRepository availabilityRepository)
+        public AvailabilityService(IMapper mapper, IAvailabilityRepository availabilityRepository, IUserRepository userRepository)
         {
             _mapper = mapper;
             _availabilityRepository = availabilityRepository;
+            _userRepository = userRepository;
         }
 
         public async Task<int> CreateAvailability(CreateAvailabilityDto model)
         {
+            int activeTimetableId = await _userRepository.GetCurrentActiveTimetable();
             var availability = _mapper.Map<Availability>(model);
+            availability.TimetableId = activeTimetableId;
             await _availabilityRepository.AddAsync(availability);
             return availability.Id;
         }
-
     }
 }
