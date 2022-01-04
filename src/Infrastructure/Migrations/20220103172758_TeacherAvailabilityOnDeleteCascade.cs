a84c5afb20e95830dc29b8b38df565e1e3497d25
ogrodowczyko <ollo9998@gmail.com>
2022-01-04 10:36:33 +0100

    Migration with cascade teacher deleting

---
+++ src/Infrastructure/Migrations/20220103172758_TeacherAvailabilityOnDeleteCascade.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class TeacherAvailabilityOnDeleteCascade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Availabilities_Teachers_TeacherId",
+                table: "Availabilities");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Availabilities_Teachers_TeacherId",
+                table: "Availabilities",
+                column: "TeacherId",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Availabilities_Teachers_TeacherId",
+                table: "Availabilities");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Availabilities_Teachers_TeacherId",
+                table: "Availabilities",
+                column: "TeacherId",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
